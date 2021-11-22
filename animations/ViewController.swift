68237607d815535e0806c1ba13f24bad903bab4d
Taras Evchenko <taras.evchenko@gmail.com>
2021-11-22 13:35:31 +0400

    add constraint animations

--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var pinkViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pinkView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func anumtionButtonTapped(_ sender: Any) {
+        UIView.animate(
+            withDuration: 3,
+            delay: 0,
+            options: [],
+            
+            animations: { [weak self] in
+                self?.pinkViewBottomConstraint.constant = 10
+                self?.view.layoutIfNeeded()
+            },
+            completion: nil)
+        
+    }
+    
+    private func rotate() {
+        pinkView.transform = pinkView.transform.rotated(by: .pi / 2)
+    }
+    
+    private func scale() {
+        pinkView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+    }
+    
 }
-
